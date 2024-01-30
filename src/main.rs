2b7a2b54bbd3e73f6442ca227d265e7723111996
Miguel Villa Floran <miguel.villafloran@gmail.com>
2024-01-29 19:02:27 -0800

    Config files

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std] // Disable the standard library
+
+// Include
+mod kernel;
+mod drivers;
+mod utils;
+mod board;
+
+// Entry point of the operating system
+fn main() {
+  // Initialize board-specific peripherals
+  board::initialization::init();
+
+  // Initialize the drivers
+  drivers::timer::init();
+  drivers::gpio::init();
+  drivers::uart::init();
+
+  // Initialize the kernel
+  kernel::entry::init();
+
+  // Start the scheduler
+  kernel::scheduler::start();
+
+  // How did we get here? Hang the processor if somehow reached.
+  loop {}
+}
